b01df8040e7841f10a12dd90fd22d3bbc4e00ce3
Emil <emil19@users.noreply.github.com>
2020-04-04 23:00:51 +0200

    Add fake user generator (for development)

---
+++ accounts/management/commands/addfakeusers.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand, CommandError
+
+# from django.utils import timezone
+# import datetime
+from django.contrib.auth import get_user_model
+from faker import Faker
+
+# YEAR = timezone.now().year
+
+
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    help = (
+        "Inserts some default user data to the database. Useful for local environments."
+    )
+
+    def handle(self, *args, **options):
+        fake = Faker("sv-SE")
+        fake.seed_instance(69)
+
+        for i in range(80):
+            fn = fake.first_name()
+            ln = fake.last_name()
+            username = (fn[:3] + ln[:2] + str(fake.random_int(max=999))).lower()
+            user, created = User.objects.get_or_create(username=username)
+            user.name = f"{fn} {ln}"
+            user.save()
