c4a02110a003646be7524e631144cacff5c85aa1
Oskar Arensmeier <oskar.arensmeier@outlook.com>
2024-07-01 10:40:36 +0200

    slice must be last

--- topchart/views.py
+++ topchart/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
 
 from django.contrib.auth import get_user_model
-from django.db.models import Sum
-from django.db.models.functions import Coalesce
 
 User = get_user_model()
-
 
 def index(request):
     search = request.GET.get("search", "")
@@ -13,13 +10,14 @@ def index(request):
     admin = request.user and request.user.is_staff
 
     users = User.objects.order_by("placing").all()
-    if not admin:
-        users = users.filter(points__gt=0)[:20]
 
     if search != "":
         name_filtered = users.filter(name__icontains=search)
         username_filtered = users.filter(username__icontains=search)
         users = name_filtered.union(username_filtered)
+
+    if not admin:
+        users = users.filter(points__gt=0)[:20]
 
     return render(
         request, "topchart/index.html", dict(users=users, filter_search=search)
