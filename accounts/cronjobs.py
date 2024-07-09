20cebdf5d0fb52167700ce65c3a688d51f171646
Oskar Arensmeier <oskar.arensmeier@outlook.com>
2024-07-09 12:15:29 +0200

    update points for all users

--- accounts/cronjobs.py
+++ accounts/cronjobs.py
@@ -6,7 +6,7 @@ from .utils import update_user_placings
 
 @cron(0, 0, -1, -1, -1)
 def update_points(num):
-    for user in User.objects.filter(is_staff=False).all():
+    for user in User.objects.filter().all():
         user.update_points()
     update_user_placings()
 
