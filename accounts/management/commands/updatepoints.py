f35759c5b67d0d5bba230728f61af470df247b14
Oskar Arensmeier <oskar.arensmeier@outlook.com>
2024-07-09 11:50:42 +0200

    run logic directly

--- accounts/management/commands/updatepoints.py
+++ accounts/management/commands/updatepoints.py
@@ -1,8 +1,12 @@
-from accounts.cronjobs import update_points
 from django.core.management.base import BaseCommand
+
+from accounts.models import User
+from accounts.utils import update_user_placings
 
 class Command(BaseCommand):
     help = 'Re-calculate points and placings for all users'
 
     def handle(self, *args, **options):
-        update_points(None)
+        for user in User.objects.filter(is_staff=False).all():
+            user.update_points()
+        update_user_placings()
