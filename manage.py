fb2bec2ee6406e718a0dccfae08d8a99f87ada3a
Andreas B <6439218+YouKnowBlom@users.noreply.github.com>
2025-07-03 18:57:05 +0200

    Default to development settings in manage.py

--- manage.py
+++ manage.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-if __name__ == "__main__":
-    if "DJANGO_SETTINGS_MODULE" not in os.environ:
-        raise Exception("DJANGO_SETTINGS_MODULE not set")
 
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fadderjobb.settings_development")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,3 +16,7 @@ if __name__ == "__main__":
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
